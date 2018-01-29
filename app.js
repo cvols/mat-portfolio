254f573e9c83d4250cc51a7b901706f0d9ba23a8
cvols <chris.volonnino@gmail.com>
2018-01-29 15:44:07 -0500

    added projects

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ $(document).ready(function () {
         fullWidth: true
     });
 
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+
     // Fire off toast
     //Materialize.toast('Hello World', 3000);
 
